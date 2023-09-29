8a3332b825c04d882f00781545cb7ecbb815f505
Jacob Hofer <jacobhofer1@gmail.com>
2023-09-28 21:30:12 -0600

    Update documentation and formatting

--- dbscan.py
+++ dbscan.py
@@ -1,17 +1,27 @@
 import pandas as pd
 import numpy as np
 
-
 class DBScan:
+    # Creates a new instance of DBScan
+    # minPts denotes the minimum number of neighbor points to denote a core point
+    # epsilon denotes the maximum distance between two points for them to still be considered in a neighborhood
+    # noiseValue denotes the cluster value to assign to noise points
     def __init__(self, minPts: int, epsilon: float, noiseValue: int = -1) -> None:
         self.__minPts = minPts
         self.__epsilon = epsilon
         self.__noiseValue = noiseValue
 
+    # Runs the DBScan algorithm on a given dataFrame
+    # dataFrame denotes the data to run DBScan on
+    # Returns a tuple containing the cluster assignments, the list of core points, list of border points, and list of noise points
     def runAlgorithm(self, dataFrame: pd.DataFrame):
+        # Reset our list of core points
         self.__corePts = []
 
+        # Set our list of all data points
         self.__observations = dataFrame.values
+
+        # Calculate our neighborhoods
         self.__neighborhoods = DBScan.__computeNeighborhoods(dataFrame, self.__epsilon)
 
         # Assignment denotes unassigned cluster (useful for coloring noise points)
@@ -19,18 +29,24 @@ class DBScan:
 
         # Find core points
         for i, observation in enumerate(self.__observations):
+            # Check we meet the minimum number of points
             if len(self.__neighborhoods[i]) >= self.__minPts:
                 self.__corePts.append(i)
 
         # Assign clusters
         self.__curCluster = 1
         for i in self.__corePts:
+            # Check the point is not already assigned
             if self.__assignments[i] != self.__noiseValue:
                 continue
+            # Assign the cluster
             self.__assignments[i] = self.__curCluster
+            # Assign clusters of nearby points
             self.__densityConnected(i)
+            # Go onto the next cluster
             self.__curCluster += 1
 
+        # Calculate our noise and border points
         noisePts = []
         borderPts = []
         for i, observation in enumerate(self.__observations):
@@ -43,7 +59,10 @@ class DBScan:
 
         return (self.__assignments, self.__corePts, borderPts, noisePts)
 
+    # Assigns clusters to points connected to a given points x
     def __densityConnected(self, x):
+
+        # Performs a breadth-first search of nearby eligible points
         visited = set()
         toVisit = []
 
@@ -60,16 +79,18 @@ class DBScan:
 
     # Static methods
 
+    # Returns the squared euclidian distance between points x1 and x2
     def __distSquared(x1, x2):
         # Euclidian distance squared
         total = 0
-        for x1i in x1:
-            for x2i in x2:
-                total += (x1i-x2i)^2
+        for i in range(len(x1)):
+            total += (x1[i] - x2[i]) * (x1[i] - x2[i])
         return total
 
+    # Computes the set of epsilon-neighborhoods for every point in a given dataframe
     def __computeNeighborhoods(dataFrame: pd.DataFrame, epsilon: float):
         observations = dataFrame.values
+
         neighborhoods = []
         epsilonSquared = epsilon * epsilon
         # Loop over each data point
--- kmeans.py
+++ kmeans.py
@@ -1,13 +1,18 @@
 import pandas as pd
 import numpy as np
 import random
+import math
 
 
+# Calculates clusters on a given data set using k-means
+# dataFrame denotes the data to perform clustering on
+# k denotes the number of clusters to find
+# epsilon denotes the change threshold to determine the algorithm's termination
 def kMeans(dataFrame: pd.DataFrame, k: int, epsilon: float):
     assignments = []
     observations = dataFrame.values
 
-    # Create random assignments
+    # Create random assignments of all points
     for _ in observations:
         assignments.append(random.randint(0, k - 1))
 
@@ -45,6 +50,10 @@ def kMeans(dataFrame: pd.DataFrame, k: int, epsilon: float):
     return (centroids, assignments)
 
 
+# Computes the centroid of a desired cluster, means-based
+# observations represents the set of all points in the dataset
+# curAssignments represents the current cluster assignments for all points
+# cluster denotes the specific cluster to compute the centroid for
 def computeCentroid(observations: np.ndarray, curAssignments: list, cluster: int):
     # Center begins at origin
     center = np.zeros(observations.shape[1])
@@ -65,9 +74,13 @@ def computeCentroid(observations: np.ndarray, curAssignments: list, cluster: int
     return center
 
 
+# Finds the closest centroid to a given point
+# observation denotes the point to find a centroid for
+# centroids is the list of centroid points
 def closestCentroid(observation, centroids):
     closestIndex = 0
     minDist = np.inf
+    # Check every centroid to find the closest
     for i, centroid in enumerate(centroids):
         curDist = dist(observation, centroid)
         # Require strict inequality, so that lower indexed centroids are prefered
@@ -79,5 +92,8 @@ def closestCentroid(observation, centroids):
 
 
 def dist(x1, x2):
-    # Euclidian distance
-    return np.sqrt(np.sum(np.square(x1 - x2)))
+    # Euclidian distance squared
+    total = 0
+    for i in range(len(x1)):
+        total += (x1[i] - x2[i]) * (x1[i] - x2[i])
+    return math.sqrt(total)
